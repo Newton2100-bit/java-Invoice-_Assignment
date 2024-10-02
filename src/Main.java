fe96cb0fa50f695bf00ad914da97127040e7e078
Newton <mwauranewton99@gmail.com>
2024-10-02 18:07:08 +0300

    Update commit for the running code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("***************************C026-01-2100/2023***************************");
+        System.out.println("#99 Acs Building Gilmore Ave. New Mla.. Queson City * Tel. No: 123-4567");
+        System.out.println("        VAT REG. TIN 098-765 4321-0000");
+        System.out.println("            SALES INVOICE");
+        System.out.println("sold to:NEWTON IRUNGU MWAURA      Date:28/04/2020");
+        System.out.println("Address:private bag               Conatact Number:0707191438");
+        System.out.println();
+        System.out.println();
+      //  System.out.println();
+        System.out.println("=========================================================================");
+        System.out.println("QTY  |  Item Description  |  Unit Price  |  Amount");
+        System.out.println("=========================================================================");
+        System.out.println("4    |  Pencil            |  20          |  80.00");
+        System.out.println("=========================================================================");
+        System.out.println("10   |  Duster            |  50          |  500.00");
+        System.out.println("=========================================================================");
+        System.out.println("9    |  Pens              |  30          |  270.00");
+        System.out.println("=========================================================================");
+        System.out.println("10   |  Crayon            |  80          |  800.00");
+        System.out.println("=========================================================================");
+        System.out.println("                          |Sub Total     |  1650.00");
+        System.out.println("                          ==========================                     ");
+        System.out.println("                          |VAT(16%)      |  264.00");
+        System.out.println("                          ==========================                     ");
+        System.out.println("                          |Total Amount  |  1914.00");
+        System.out.println("                          ==========================                     ");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
